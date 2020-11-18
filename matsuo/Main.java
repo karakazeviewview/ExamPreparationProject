d264a682bfeadf75b8014c6e0c827ad77e1203c2
matsuo <mitsuru.matsuo.great@gmail.com>
2020-11-18 11:41:30 +0900

    解答を追加しました

---
+++ matsuo/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		int count=0;
+		for(int i=0;i<8;i++){
+		for(int j=0;j<36;j=j+3){
+			if(j%9==0){
+					continue;
+				}count++;
+	}}	
+		System.out.println(count);
+	}
+}
---
+++ matsuo/While.java
@@ -0,0 +1,6 @@
+public class While{
+	public static void main(String[] args){
+		:
+	}
+}
+
